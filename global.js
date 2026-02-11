0154dbe3db990acee38f3e9d702202ceaac94c26
Orxan Kerim <mrorkhankarimli@gmail.com>
2026-02-11 15:15:24 +0400

    Translate codes added

--- global.js
+++ global.js
@@ -12,8 +12,10 @@ header.classList.remove('scrolled');
 const burger = document.querySelector('.burger-menu');
 const mobileMenu = document.getElementById('mobileMenu');
 const icon = burger.querySelector('i');
+
 burger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');  
+  mobileMenu.classList.toggle('active');
+
   icon.classList.toggle('fa-bars');
   icon.classList.toggle('fa-xmark');
 });
