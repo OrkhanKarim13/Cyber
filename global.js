d08df316c321a42744304144d1879c3e132bdfa0
Orxan Kerim <mrorkhankarimli@gmail.com>
2026-02-11 13:33:46 +0400

    header-background fixed

--- global.js
+++ global.js
@@ -1,24 +1,19 @@
-const header1 = document.querySelector('.header');
-
+const header = document.querySelector('.header');
 
 window.addEventListener('scroll', () => {
 if (window.scrollY > 50) {
-header1.classList.add('scrolled');
+header.classList.add('scrolled');
 } else {
-header1.classList.remove('scrolled');
+header.classList.remove('scrolled');
 }
 });
-
-
 
 
 const burger = document.querySelector('.burger-menu');
 const mobileMenu = document.getElementById('mobileMenu');
 const icon = burger.querySelector('i');
-
 burger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
-
+  mobileMenu.classList.toggle('active');  
   icon.classList.toggle('fa-bars');
   icon.classList.toggle('fa-xmark');
 });
